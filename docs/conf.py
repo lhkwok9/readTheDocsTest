30f6f6643ad4518d72f66ad688563d1b92d4f0dd
lhkwok9 <s2007klhwaiommk@gmail.com>
2026-04-09 11:25:35 +0800

    fix theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = 'alpha 1.1e'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
